0e17bcd50294513c7f6d342bf5f5e5a48ff2d210
Josh <josh24beck@gmail.com>
2020-03-01 10:55:11 -0800

    Success 100

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <fstream>
 #include "../includes_usr/fileIO.h"
 using namespace std;
 /* clears, then loads books from the file filename
@@ -7,6 +9,58 @@ using namespace std;
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
+	fstream file;
+	file.open(filename);
+	book libBook;
+	string line;
+
+	if (!file.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	books.clear();
+	while (!file.eof()) {
+
+		getline(file, line, ',');
+		//when we get to last line in the file we need to break out of the loop
+		if (line == "") {
+			break;
+		}
+		//string to int
+		libBook.book_id = stoi(line);
+
+		getline(file, line, ',');
+		libBook.title = line;
+
+		getline(file, line, ',');
+		libBook.author = line;
+
+		getline(file, line, ',');
+		int state = stoi(line);
+		if (state == 0) {
+			libBook.state = UNKNOWN;
+		}
+		else if (state == 1) {
+			libBook.state = IN;
+		}
+		else {
+			libBook.state = OUT;
+		}
+
+		//last element in line
+		getline(file, line, '\n');
+		libBook.loaned_to_patron_id = stoi(line);
+
+		books.push_back(libBook);
+		//if (books.empty()) {
+		//	return NO_BOOKS_IN_LIBRARY;
+		//}
+	}
+	if (books.empty()) {
+		return NO_BOOKS_IN_LIBRARY;
+	}
+
+	file.close();
 	return SUCCESS;
 }
 
@@ -17,6 +71,37 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * */
 int saveBooks(std::vector<book> &books, const char* filename)
 {
+	ofstream ofile;
+	ofile.open(filename);
+	string toOut;
+
+	//place-holders because I'm dumb and get confused
+	string bookid;
+	string booktitle;
+	string bookauthor;
+	string bookstate;
+	string patronid;
+
+	if (!ofile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	if (books.empty()) {
+		return NO_BOOKS_IN_LIBRARY;
+	}
+
+	for (int i = 0; i < books.size(); i++) {
+		bookid = to_string(books[i].book_id);
+		booktitle = books[i].title;
+		bookauthor = books[i].author;
+		bookstate = to_string(books[i].state);
+		patronid = to_string(books[i].loaned_to_patron_id);
+
+		toOut = bookid + "," + booktitle + "," + bookauthor + "," + bookstate + "," + patronid;
+		ofile << toOut << endl;
+	}
+
+	ofile.close();
 	return SUCCESS;
 }
 
@@ -27,6 +112,42 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename)
 {
+	fstream file;
+	file.open(filename);
+
+	if (!file.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	patrons.clear();
+	while (!file.eof()) {
+		string line;
+		patron person;
+
+		getline(file, line, ',');
+		//when we get to last, blank line we need to break out of while
+		if (line == "") {
+			break;
+		}
+		person.patron_id = stoi(line);
+
+		getline(file, line, ',');
+		person.name = line;
+
+		//last element in line
+		getline(file, line, '\n');
+		person.number_books_checked_out = stoi(line);
+
+		patrons.push_back(person);
+		//if (patrons.empty()) {
+		//	return NO_PATRONS_IN_LIBRARY;
+		//}
+	}
+	if (patrons.empty()) {
+		return NO_PATRONS_IN_LIBRARY;
+	}
+
+	file.close();
 	return SUCCESS;
 }
 
@@ -37,5 +158,32 @@ int loadPatrons(std::vector<patron> &patrons, const char* filename)
  * */
 int savePatrons(std::vector<patron> &patrons, const char* filename)
 {
+	ofstream ofile;
+	ofile.open(filename);
+	string toOut;
+
+	//place-holders once again
+	string patronid;
+	string patronName;
+	string patronsBooks;
+
+	if (!ofile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	if (patrons.empty()) {
+		return NO_PATRONS_IN_LIBRARY;
+	}
+
+	for (int i = 0; i < patrons.size(); i++) {
+		patronid = to_string(patrons[i].patron_id);
+		patronName = patrons[i].name;
+		patronsBooks = to_string(patrons[i].number_books_checked_out);
+
+		toOut = patronid + "," + patronName + "," + patronsBooks;
+		ofile << toOut << endl;
+	}
+
+	ofile.close();
 	return SUCCESS;
 }
--- library/library.cpp
+++ library/library.cpp
@@ -13,12 +13,17 @@ using namespace std;
 //NOTE: also make sure you save patron and book data to disk any time you make a change to them
 //NOTE: for files where data is stored see constants.h BOOKFILE and PATRONFILE
 
+//global vectors
+vector<book> books;
+vector<patron> patrons;
+
 /*
  * clear books and patrons containers
  * then reload them from disk 
  */
 void reloadAllData(){
-
+	loadBooks(books, BOOKFILE.c_str());
+	loadPatrons(patrons, PATRONFILE.c_str());
 }
 
 /* checkout a book to a patron
@@ -42,6 +47,46 @@ void reloadAllData(){
  *         TOO_MANY_OUT patron has the max number of books allowed checked out
  */
 int checkout(int bookid, int patronid){
+	reloadAllData();
+	int booksOut = howmanybooksdoesPatronHaveCheckedOut(patronid);
+
+	//check if patron is enrolled
+	if (booksOut == PATRON_NOT_ENROLLED) {
+		return PATRON_NOT_ENROLLED;
+	}
+
+	//check if patron has max number of books out
+	if (booksOut == MAX_BOOKS_ALLOWED_OUT) {
+		return TOO_MANY_OUT;
+	}
+
+	//check if book is in collection, record location if it is
+	bool inCol = false;
+	for (int i = 0; i < books.size(); i++) {
+		if (bookid == books[i].book_id) {
+			inCol = true;
+
+			//checkout process: book
+			books[i].loaned_to_patron_id = patronid;
+			books[i].state = OUT;
+		}
+	}
+
+	if (!inCol) {
+		return BOOK_NOT_IN_COLLECTION;
+	}
+
+	booksOut += 1;
+	//checkout process: patron
+	for (int i = 0; i < patrons.size(); i++) {
+		if (patrons[i].patron_id == patronid) {
+			patrons[i].number_books_checked_out = booksOut;
+		}
+	}
+
+	//save
+	savePatrons(patrons, PATRONFILE.c_str());
+	saveBooks(books, BOOKFILE.c_str());
 	return SUCCESS;
 }
 
@@ -58,6 +103,34 @@ int checkout(int bookid, int patronid){
  * 		   BOOK_NOT_IN_COLLECTION
  */
 int checkin(int bookid){
+	reloadAllData();
+
+	//check if book is in collection
+	bool inCol = false;
+	for (int i = 0; i < books.size(); i++) {
+		if (books[i].book_id == bookid) {
+			inCol = true;
+		}
+	}
+	if (!inCol) {
+		return BOOK_NOT_IN_COLLECTION;
+	}
+
+	//fix patron data
+	int loc;
+	for (int i = 0; i < books.size(); i++) {
+		if (books[i].book_id == bookid) {
+			loc = i;
+		}
+	}
+	for (int i = 0; i < patrons.size(); i++) {
+		if (patrons[i].patron_id == books[loc].loaned_to_patron_id) {
+			patrons[i].number_books_checked_out--;
+		}
+	}
+	//change book state, change loaned to patron to no one
+	books[loc].state = IN;
+	books[loc].loaned_to_patron_id = NO_ONE;
 	return SUCCESS;
 }
 
@@ -71,7 +144,16 @@ int checkin(int bookid){
  *    the patron_id of the person added
  */
 int enroll(std::string &name){
-	return 0;
+	reloadAllData();
+
+	//initialize person
+	patron person;
+	person.name = name;
+	person.number_books_checked_out = 0;
+	person.patron_id = patrons.size();
+	patrons.push_back(person);
+	savePatrons(patrons, PATRONFILE.c_str());
+	return person.patron_id;
 }
 
 /*
@@ -80,7 +162,8 @@ int enroll(std::string &name){
  * 
  */
 int numbBooks(){
-	return 0;
+	reloadAllData();
+	return books.size();
 }
 
 /*
@@ -88,7 +171,8 @@ int numbBooks(){
  * (ie. if 3 patrons returns 3)
  */
 int numbPatrons(){
-	return 0;
+	reloadAllData();
+	return patrons.size();
 }
 
 /*the number of books patron has checked out
@@ -97,7 +181,20 @@ int numbPatrons(){
  *        or PATRON_NOT_ENROLLED         
  */
 int howmanybooksdoesPatronHaveCheckedOut(int patronid){
-	return 0;
+	int booksOut;
+	bool enrolled = false;
+	for (int i = 0; i < patrons.size(); i++) {
+		if (patronid == patrons[i].patron_id){
+			enrolled = true;
+			booksOut = patrons[i].number_books_checked_out;
+		}
+	}
+
+	if(!enrolled) {
+		return PATRON_NOT_ENROLLED;
+	}
+
+	return booksOut;
 }
 
 /* search through patrons container to see if patronid is there
@@ -107,6 +204,16 @@ int howmanybooksdoesPatronHaveCheckedOut(int patronid){
  *         PATRON_NOT_ENROLLED no patron with this patronid
  */
 int whatIsPatronName(std::string &name,int patronid){
+	bool enrolled = false;
+	for (int i = 0; i < patrons.size(); i++) {
+		if (patronid == patrons[i].patron_id) {
+			name = patrons[i].name;
+			enrolled = true;
+		}
+	}
+	if (!enrolled) {
+		return PATRON_NOT_ENROLLED;
+	}
 	return SUCCESS;
 }
 
